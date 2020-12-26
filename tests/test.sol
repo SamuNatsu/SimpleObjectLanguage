537b6d83d6ae17b3fbd4e14befab591f79e1722d
SamuNatsu <49141130+SamuNatsu@users.noreply.github.com>
2020-12-26 10:22:16 +0800

    Create test.sol

---
+++ tests/test.sol
@@ -0,0 +1,35 @@
+{
+    version = "1.0.0",
+    settings = {
+        volume = {
+            main = "100",
+            fx = "100",
+            environment = "100",
+            vocal = "100"
+        },
+        graphic = {
+            fps = "120",
+            resolution = "1920x1080",
+            bright = "1.2",
+            quality = "3"
+        },
+        game = {
+            autosave = {
+                status = "true",
+                interval = "600"
+            }
+        }
+    },
+    server = [
+        {
+            name = "My Server",
+            ip = "114.514.191.81",
+            port = "1919"
+        },
+        {
+            name = "Official Server",
+            ip = "svr.sgames.net",
+            port = "12345"
+        }
+    ]
+}
